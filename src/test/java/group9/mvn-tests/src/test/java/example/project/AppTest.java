a918c740bf6c375417967b869029eac5622235d7
Asken59 <michaelask@hotmail.se>
2023-02-08 11:14:12 +0100

    feat/#10 Added basic mvn project to test on 	new file:   pom.xml 	new file:   src/main/java/example/project/App.java 	new file:   src/test/java/example/project/AppTest.java

---
+++ src/test/java/group9/mvn-tests/src/test/java/example/project/AppTest.java
@@ -0,0 +1,28 @@
+package example.project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Unit test for example project
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithSucceed()
+    {
+        Assertions.assertTrue( true );
+    }
+
+
+    @Test
+    public void shouldAnswerWithFail()
+    {
+        Assertions.assertTrue( true );
+    }
+
+}
