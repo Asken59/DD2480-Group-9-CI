0744e92f5e7cb9f09e940ac2abca1325b7598d6d
AdamGSchmidt <adam.schmidt.nacka@gmail.com>
2023-02-10 10:19:20 +0100

    feat/#52 Added subproject for compilation failure test

---
+++ mvn-tests/mvn-fail-compile/src/main/java/example/project/AppFail.java
@@ -0,0 +1,14 @@
+package example.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppFail
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        this will not compile
+    }
+}
